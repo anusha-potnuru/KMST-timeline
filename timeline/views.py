47a9157d211b40751359c6a397708bcecfc4bb7c
Saketh <alurusaisaketh@gmail.com>
2018-10-01 20:10:44 +0530

    Add predicate and object queries

--- timeline/views.py
+++ timeline/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 from SPARQLWrapper import SPARQLWrapper, JSON
 # Create your views here.
-def index(request):
-	query_str = get_query()
-	sparql = SPARQLWrapper('https://query.wikidata.org/sparql')
-	sparql.setQuery(query_str)
-	sparql.setReturnFormat(JSON)
-	results = sparql.query().convert()
-	result = results['results']['bindings']
-	print(type(result))
-	return render(request, 'timeline/file.html',{'content':result})
-
 
 def get_query():
-	return ("""
+	query = """
 	SELECT ?item ?itemLabel ?launchdate (SAMPLE(?image) AS ?image)
 	WHERE
 	{
@@ -25,4 +15,49 @@ def get_query():
 	    ?item wdt:P137 wd:Q23548.
 	}
 	GROUP BY ?item ?itemLabel ?launchdate
-	""")
+	"""
+	return query
+
+def get_predicate_query():
+	query = """
+	SELECT DISTINCT ?p ?o
+	WHERE
+	{
+		?item wdt:P31 wd:Q26529 .
+		?item wdt:P137 wd:Q23548 .
+		?item ?p ?o.
+	}
+	"""
+	return query
+
+def exec_query():
+	query_str = get_query()
+	sparql = SPARQLWrapper('https://query.wikidata.org/sparql')
+	sparql.setQuery(query_str)
+	sparql.setReturnFormat(JSON)
+	results = sparql.query().convert()
+	result = results['results']['bindings']
+	return result
+
+def get_predicates_and_objects():
+	query_str = get_predicate_query()
+	sparql = SPARQLWrapper('https://query.wikidata.org/sparql')
+	sparql.setQuery(query_str)
+	sparql.setReturnFormat(JSON)
+	results = sparql.query().convert()
+	result_uris = results['results']['bindings']
+	res_predicates = []
+	for uri in result_uris:
+		res_predicates.append(uri['p']['value'])
+	res_objects = []
+	for uri in result_uris:
+		res_objects.append(uri['o']['value'])
+	return (res_predicates,res_objects)
+
+
+def index(request):
+	predicates,objects = get_predicates_and_objects()
+	result = exec_query()
+	return render(request, 'timeline/file.html',{'content':result, 'predicates': predicates, 'objects':objects})
+
+
